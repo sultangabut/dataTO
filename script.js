7e50500a3545d45717826426c6e4d8e11f0d70b4
Sultan Gabut <156124971+sultangabut@users.noreply.github.com>
2024-01-10 06:37:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function submitForm() {
   const formData = new FormData(document.getElementById("myForm"));
   const jsonData = {};
   formData.forEach((value, key) => {
-    jsonData[key] = value;
+ jsonData[b3113293f86b53a14b9d3a6ddca4779826b2e4c8] = value;
   });
 
   // Kirim data ke spreadsheet menggunakan Google Sheets API atau backend lainnya
